830eacd7074c07c7dc0926d927c1dd39f63ac2f4
Benyi Wang <bwang@rhapsody.com>
2017-01-30 11:14:20 -0800

    WIP: new AppConfig implementation

--- src/main/scala/com/napster/bi/config/AppConfig.scala
+++ src/main/scala/com/napster/bi/config/AppConfig.scala
@@ -2,12 +2,97 @@ package com.napster.bi.config
 
 import java.util.concurrent.TimeUnit
 
+import com.napster.bi.config.AppConfig.Root
 import com.typesafe.config.{Config, ConfigValueType}
 
-import scala.language.dynamics
 import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
+import scala.language.dynamics
+import scala.reflect.ClassTag
 
 class AppConfigException(msg: String) extends Exception(msg)
+
+object NewAppConfig {
+
+  def apply(name: String, config: Config) = Container(name, "", config.getConfig(name))
+
+  trait ConfigReader[T] {
+    def apply(path: String, config: Config): T
+  }
+
+  implicit object IntReader extends ConfigReader[Int] {
+    def apply(path: String, config: Config): Int = config.getInt(path)
+  }
+
+  implicit object IntSeqReader extends ConfigReader[Seq[Int]] {
+    def apply(path: String, config: Config): Seq[Int] =
+      config.getIntList(path).map(_.intValue())
+  }
+
+  trait Node extends Dynamic {
+
+    def apply(key: String): Node
+
+    def selectDynamic(key: String): Node = apply(key)
+
+    def as[T: ConfigReader]: T
+
+    def int: Int = as[Int]
+
+//    def bool: Boolean = as[Boolean]
+//
+//    def long: Long = as[Long]
+//
+//    def double: Double = as[Double]
+//
+//    def float: Float = as[Float]
+//
+//    def string: String = as[String]
+
+  }
+
+  case class Value(name: String, path: String, parent: Config) extends Node {
+
+    def as[T: ConfigReader]: T = {
+      val reader = implicitly[ConfigReader[T]]
+      reader(name, parent)
+    }
+
+    override def apply(key: String) =
+      throw new IllegalArgumentException(s"$path is a value which cannot have field $key.")
+
+  }
+
+  case class Container(name: String, path: String, self: Config) extends Node {
+
+    private[this] val cache = collection.mutable.Map[String, Node]()
+
+    private def isObject(key: String) =
+      self.getValue(key).valueType() == ConfigValueType.OBJECT
+
+    def as[T: ConfigReader]: T = self.asInstanceOf[T]
+
+    private def create(key: String) = {
+      val childPath = s"${path}.${key}"
+      if (isObject(key))
+        Container(key, childPath, self.getConfig(key))
+      else
+        Value(key, childPath, self)
+    }
+
+    override def apply(key: String): Node =
+      if (self.hasPath(key))
+        cache.getOrElseUpdate(key, create(key))
+      else
+        throw new IllegalArgumentException(s"Cannot find the key $key in $path.")
+
+  }
+
+  implicit def node2Int(nd: Node): Int = nd.as[Int]
+
+//  implicit def node2String(nd: Node): String = nd.as[String]
+
+}
 
 object AppConfig {
 
@@ -27,9 +112,9 @@ object AppConfig {
     override lazy val strings = parent.config.getStringList(name).asScala
   }
 
-//  case class Array(name: String, parent: Node) extends AppConfig {
-//    def seq[T: TypeTag] =
-//  }
+  //  case class Array(name: String, parent: Node) extends AppConfig {
+  //    def seq[T: TypeTag] =
+  //  }
 
   trait ConfigObject extends AppConfig {
     def getField(field: String): AppConfig =
@@ -68,10 +153,15 @@ trait AppConfig extends Dynamic {
   def config: Config = error
 
   def int: Int = error
+
   def string: String = error
+
   def long: Long = error
+
   def bool: Boolean = error
+
   def duration(unit: TimeUnit): Long = error
+
   def size: Long = error
 
   def strings: Seq[String] = error
--- src/test/scala/com/napster/bi/config/AppConfigSpec.scala
+++ src/test/scala/com/napster/bi/config/AppConfigSpec.scala
@@ -1,8 +1,5 @@
 package com.napster.bi.config
 
-import java.util.concurrent.TimeUnit
-
-import com.napster.bi.config.AppConfig._
 import com.typesafe.config.ConfigFactory
 import org.scalatest._
 
@@ -11,7 +8,7 @@ class AppConfigSpec extends WordSpec with Matchers {
   "AppConfig" should {
 
     def appConf(config: String, rootName: String = "my-app") = {
-      AppConfig(rootName, ConfigFactory.parseString(config))
+      NewAppConfig(rootName, ConfigFactory.parseString(config))
     }
 
     "get a config value" when {
@@ -19,27 +16,30 @@ class AppConfigSpec extends WordSpec with Matchers {
         appConf("my-app { country = US }").country.string should be("US")
       }
       "the value is Int" in {
-        appConf("my-app { age = 23 }").age.int should be(23)
+        //        def plus5(age: Int) = age + 5
+        //        plus5(appConf("my-app { age = 23 }").age) should be(23)
+        //
+        appConf("my-app { age = 23 }").age * 3L should be(66)
       }
       "the value is Long" in {
         appConf("my-app { population = 15 }").population.long should be(15L)
       }
-      "the value is Boolean" in {
-        appConf("my-app { isMale = false }").isMale.bool should be(false)
-      }
-      "the value is Duration" in {
-        appConf("my-app { interval = 5m }").interval.duration(TimeUnit.SECONDS) should be(300)
-      }
-      "the value is Size" in {
-        appConf("my-app { maxSize = 5m }").maxSize.size should be(5 * 1024 * 1024)
-      }
-      "the value is a list of strings" in {
-        val result = appConf("my-app { topics = [ topic-1, topic-2 ] }").topics.strings
-        result should contain allOf("topic-1", "topic-2")
-      }
-      "the key is not valid identifier" in {
-        appConf("my-app { is-male = false }")("is-male").bool should be(false)
-      }
+      //      "the value is Boolean" in {
+      //        appConf("my-app { isMale = false }").isMale.bool should be(false)
+      //      }
+      //      "the value is Duration" in {
+      //        appConf("my-app { interval = 5m }").interval.duration(TimeUnit.SECONDS) should be(300)
+      //      }
+      //      "the value is Size" in {
+      //        appConf("my-app { maxSize = 5m }").maxSize.size should be(5 * 1024 * 1024)
+      //      }
+      //      "the value is a list of strings" in {
+      //        val result = appConf("my-app { topics = [ topic-1, topic-2 ] }").topics.strings
+      //        result should contain allOf("topic-1", "topic-2")
+      //      }
+      //      "the key is not valid identifier" in {
+      //        appConf("my-app { is-male = false }")("is-male").bool should be(false)
+      //      }
 
     }
 
@@ -80,38 +80,38 @@ class AppConfigSpec extends WordSpec with Matchers {
           }
         }""").country.US.WA.population.long should be(15L)
       }
-      "the value is Boolean" in {
-        appConf(
-          """my-app {
-          year {
-            artists {
-              recompute = false
-            }
-          }
-        }""").year.artists.recompute.bool should be(false)
-      }
+      //      "the value is Boolean" in {
+      //        appConf(
+      //          """my-app {
+      //          year {
+      //            artists {
+      //              recompute = false
+      //            }
+      //          }
+      //        }""").year.artists.recompute.bool should be(false)
+      //      }
     }
 
-    "return NonExistentValue" when {
-      val nonExistentKey = "non-existent-key"
-      "the field name doesn't exist" in {
-        val conf = appConf("my-app {}")
-        conf(nonExistentKey) should be(NonExistentValue(nonExistentKey, conf))
-      }
-      "the nested field doesn't exist" in {
-        an[AppConfigException] should be thrownBy
-          appConf(
-            """my-app {
-            country {
-              US {
-                north-west {
-                  WA = 1
-                }
-              }
-            }
-          }""").country.US("north-west").FL.int
-      }
-    }
+    //    "return NonExistentValue" when {
+    //      val nonExistentKey = "non-existent-key"
+    //      "the field name doesn't exist" in {
+    //        val conf = appConf("my-app {}")
+    //        conf(nonExistentKey) should be(NonExistentValue(nonExistentKey, conf))
+    //      }
+    //      "the nested field doesn't exist" in {
+    //        an[AppConfigException] should be thrownBy
+    //          appConf(
+    //            """my-app {
+    //            country {
+    //              US {
+    //                north-west {
+    //                  WA = 1
+    //                }
+    //              }
+    //            }
+    //          }""").country.US("north-west").FL.int
+    //      }
+    //    }
 
     "get the path" when {
       "there is only one level" in {
@@ -131,16 +131,16 @@ class AppConfigSpec extends WordSpec with Matchers {
       }
     }
 
-//    "get an array" when {
-//      "element type is string" in {
-//        appConf(
-//          """
-//            |my-app {
-//            |  list = [ "a", "b" ]
-//            |}
-//          """.stripMargin).list.seq[String] should be(Seq("a", "b"))
-//      }
-//    }
+    //    "get an array" when {
+    //      "element type is string" in {
+    //        appConf(
+    //          """
+    //            |my-app {
+    //            |  list = [ "a", "b" ]
+    //            |}
+    //          """.stripMargin).list.seq[String] should be(Seq("a", "b"))
+    //      }
+    //    }
 
   }
 }
