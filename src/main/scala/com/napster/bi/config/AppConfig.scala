ffef09e9eee2d82869aed3c28345ba703c48a6bd
Benyi Wang <bwang@napster.com>
2017-09-13 19:02:07 +0000

    change Reader to Read: because it is actually a function, it is better to use a verb. revert to all implicit def for types. The generic one in last commit didn't work.

--- src/main/scala/com/napster/bi/config/AppConfig.scala
+++ src/main/scala/com/napster/bi/config/AppConfig.scala
@@ -13,9 +13,9 @@ trait AppConfig extends Dynamic {
 
   def selectDynamic(key: String): AppConfig = apply(key)
 
-  def as[T: AppConfig.Reader]: T
+  def as[T: AppConfig.Read]: T
 
-  def asOption[T: AppConfig.Reader]: Option[T]
+  def asOption[T: AppConfig.Read]: Option[T]
 
 }
 
@@ -23,32 +23,32 @@ object AppConfig {
 
   def apply(name: String, config: Config): AppConfig = Container(name, "", config)
 
-  type Reader[T] = Function2[String, Config, T]
+  type Read[T] = Function2[String, Config, T]
 
-  implicit val intReader: Reader[Int] = (path, config) => config.getInt(path)
-  implicit val stringReader: Reader[String] = (path, config) => config.getString(path)
-  implicit val boolReader: Reader[Boolean] = (path, config) => config.getBoolean(path)
-  implicit val longReader: Reader[Long] = (path, config) => config.getLong(path)
-  implicit val doubleReader: Reader[Double] = (path, config) => config.getDouble(path)
-  implicit val floatReader: Reader[Float] = (path, config) => config.getDouble(path).toFloat
+  implicit val intReader: Read[Int] = (path, config) => config.getInt(path)
+  implicit val stringReader: Read[String] = (path, config) => config.getString(path)
+  implicit val boolReader: Read[Boolean] = (path, config) => config.getBoolean(path)
+  implicit val longReader: Read[Long] = (path, config) => config.getLong(path)
+  implicit val doubleReader: Read[Double] = (path, config) => config.getDouble(path)
+  implicit val floatReader: Read[Float] = (path, config) => config.getDouble(path).toFloat
 
-  implicit val configReader: Reader[Config] = (path, config) => config.getConfig(path)
+  implicit val configReader: Read[Config] = (path, config) => config.getConfig(path)
 
-  implicit val intSeqReader: Reader[Seq[Int]] = (path, config) => config.getIntList(path).map(_.intValue)
-  implicit val stringSeqReader: Reader[Seq[String]] = (path, config) => config.getStringList(path)
-  implicit val boolSeqReader: Reader[Seq[Boolean]] = (path, config) => config.getBooleanList(path).map(_.booleanValue())
-  implicit val longSeqReader: Reader[Seq[Long]] = (path, config) => config.getLongList(path).map(_.longValue())
-  implicit val doubleSeqReader: Reader[Seq[Double]] = (path, config) => config.getDoubleList(path).map(_.doubleValue())
-  implicit val floatSeqReader: Reader[Seq[Float]] = (path, config) => config.getDoubleList(path).map(_.toFloat)
+  implicit val intSeqReader: Read[Seq[Int]] = (path, config) => config.getIntList(path).map(_.intValue)
+  implicit val stringSeqReader: Read[Seq[String]] = (path, config) => config.getStringList(path)
+  implicit val boolSeqReader: Read[Seq[Boolean]] = (path, config) => config.getBooleanList(path).map(_.booleanValue())
+  implicit val longSeqReader: Read[Seq[Long]] = (path, config) => config.getLongList(path).map(_.longValue())
+  implicit val doubleSeqReader: Read[Seq[Double]] = (path, config) => config.getDoubleList(path).map(_.doubleValue())
+  implicit val floatSeqReader: Read[Seq[Float]] = (path, config) => config.getDoubleList(path).map(_.toFloat)
 
   case class Value(name: String, path: String, parent: Config) extends AppConfig {
 
-    override def as[T: Reader]: T = {
-      val reader = implicitly[Reader[T]]
-      reader(name, parent)
+    override def as[T: Read]: T = {
+      val read = implicitly[Read[T]]
+      read(name, parent)
     }
 
-    override def asOption[T: Reader]: Option[T] =
+    override def asOption[T: Read]: Option[T] =
       Some(as[T])
 
     override def apply(key: String) =
@@ -64,12 +64,12 @@ object AppConfig {
     private def isObject(key: String) =
       self.getValue(key).valueType() == ConfigValueType.OBJECT
 
-    override def as[T: Reader]: T = {
-      val reader = implicitly[Reader[T]]
-      reader(name, parent)
+    override def as[T: Read]: T = {
+      val read = implicitly[Read[T]]
+      read(name, parent)
     }
 
-    override def asOption[T: Reader]: Option[T] =
+    override def asOption[T: Read]: Option[T] =
       Some(as[T])
 
     private def create(key: String, childPath: String) =
@@ -88,10 +88,10 @@ object AppConfig {
   }
 
   case class NonExistAppConfig(name: String, path: String, parent: Config) extends AppConfig {
-    override def as[T: Reader]: T =
+    override def as[T: Read]: T =
       throw new NoSuchElementException(s"$path doesn't exist")
 
-    override def asOption[T: Reader]: Option[T] = None
+    override def asOption[T: Read]: Option[T] = None
 
     override def apply(key: String) =
       NonExistAppConfig(key, s"$path.$key", parent)
@@ -111,6 +111,18 @@ object AppConfig {
     def float: Float = nd.as[Float]
   }
 
-  implicit def node2[T](nd: AppConfig): T = nd.as[T]
+  implicit def node2Int(nd: AppConfig): Int = nd.as[Int]
+
+  implicit def node2String(nd: AppConfig): String = nd.as[String]
+
+  implicit def node2Boolean(nd: AppConfig): Boolean = nd.as[Boolean]
+
+  implicit def node2Long(nd: AppConfig): Long = nd.as[Long]
+
+  implicit def node2Double(nd: AppConfig): Double = nd.as[Double]
+
+  implicit def node2Float(nd: AppConfig): Float = nd.as[Float]
+
+  implicit def node2Config(nd: AppConfig): Config = nd.as[Config]
 
 }
